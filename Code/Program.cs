1bf43c0c770f03ed15658ca77491d196265030cc
AnnaKriuchkova <nura.spirina@yandex.ru>
2022-12-09 11:10:25 +0300

    Code file added

---
+++ Code/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+string [] array ={ "ёж", "сова", "ель", "уж", "крот", "медведь", "заяц", "крот", "сом"};
+
+int filteredcount = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+        {
+            filteredcount ++;
+        }
+}
+if (filteredcount == 0)
+{
+    Console.WriteLine("[]");
+}
+else
+{
+    string [] filteredArray = new string [filteredcount];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+        if(array[i].Length <= 3)
+        {
+            filteredArray[index] = array[i];
+            index = index+1;
+        }
+        }
+    Console.WriteLine(String.Join(" ",filteredArray));
+}
+
